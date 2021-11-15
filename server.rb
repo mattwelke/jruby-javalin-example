0a0e63b02a1c51a8723c033dc6282c3c103297cf
Matt Welke <mattwelke@gmail.com>
2021-11-15 01:10:04 -0500

    Remove class Person. Remove remaining refs to gem awesome_print.

--- server.rb
+++ server.rb
@@ -1,15 +1,9 @@
 require 'java' 
 require 'jruby/core_ext'
 require 'bundler/setup' 
-require 'awesome_print'
 Bundler.require
 
 java_import 'io.javalin.Javalin'
-
-class Person
-end
-
-p = Person.new
 
 app = Javalin.create.start 7000
 app.get '/' do |ctx|
